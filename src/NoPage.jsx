e76f47a95d78c94b8453d990b5be839dfcbbadb6
KarthiDreamr <karthidreamr@gmail.com>
2023-09-04 00:35:09 +0530

    modified NoPage

---
+++ src/NoPage.jsx
@@ -0,0 +1,10 @@
+function NoPage() {
+    return (
+        <div className="nopage">
+            <h1>404 - Page Not Found</h1>
+            <p>The page you are looking for does not exist.</p>
+        </div>
+    );
+}
+
+export default NoPage
